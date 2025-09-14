2b85b51ee1eaae9e51835add824b8981e3f98ca3
David Helbig <52451401+davidhelbig@users.noreply.github.com>
2025-09-14 17:57:53 +0200

    Add basic cli using flags

--- main.go
+++ main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 )
 
-const PORT = 7070
-
 func main() {
-	log.Printf("Listening on :%d", PORT)
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", PORT))
+	portPtr := flag.Int("port", 7000, "The port to listen on.")
+	binAddrPtr := flag.String("bind", "127.0.0.1", "bind to this address")
+	rootPtr := flag.String("root", ".", "The root directory to serve")
+	hostPtr := flag.String("host", "127.0.0.1", "Hostname for gopher menus")
+
+	flag.Parse()
+
+	addr := fmt.Sprintf("%v:%v", *binAddrPtr, *portPtr)
+
+	log.Printf("Listening on %s", addr)
+	ln, err := net.Listen("tcp", addr)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	hole, err := CreateFsGopherHole("./gopherhole/", "localhost", 7070)
+	hole, err := CreateFsGopherHole(*rootPtr, *hostPtr, *portPtr)
 
 	if err != nil {
 		log.Fatal("error creating gopher hole: %w", err)
